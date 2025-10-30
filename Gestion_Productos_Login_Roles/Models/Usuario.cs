c5c344377f35f12c3c2e526cbaae963bc91bff28
JulioBendz <bengutjul2@gmail.com>
2025-10-30 02:31:53 -0500

    Add Usuario model with properties for user management

---
+++ Gestion_Productos_Login_Roles/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gestion_Productos_Login_Roles.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Nombre { get; set; }
+
+        [Required, StringLength(50), Column("Usuario")]
+        public required string UsuarioNombre { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Clave { get; set; }
+
+        [Required]
+        public required string Rol { get; set; }
+    }
+}
