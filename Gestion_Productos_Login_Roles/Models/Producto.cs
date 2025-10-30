a3dddb1ef43c045be6b5d94f18924d985a0e35fe
JulioBendz <bengutjul2@gmail.com>
2025-10-30 02:32:25 -0500

    Add Producto model with properties for product management

---
+++ Gestion_Productos_Login_Roles/Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gestion_Productos_Login_Roles.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Nombre { get; set; }
+
+        [Range(0.01, 9999.99, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
+        public int Stock { get; set; }
+
+        [Display(Name = "Categoría")]
+        public int IdCategoria { get; set; }
+
+        [ForeignKey("IdCategoria")]
+        public required Categoria Categoria { get; set; }
+    }
+}
