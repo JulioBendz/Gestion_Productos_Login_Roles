a78529bbd74a63e2080220910afba4cb8ddaa4c9
JulioBendz <bengutjul2@gmail.com>
2025-10-30 02:32:14 -0500

    Add BDContext class for Entity Framework Core database context with DbSet properties for Usuarios, Categorias, and Productos

---
+++ Gestion_Productos_Login_Roles/Models/BDContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_Productos_Login_Roles.Models
+{
+    public class BDContext : DbContext
+    {
+        public BDContext(DbContextOptions<BDContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
