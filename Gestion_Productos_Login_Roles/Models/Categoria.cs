39ac7551d0ac8d9f2daa30747236db2bb4ff071e
JulioBendz <bengutjul2@gmail.com>
2025-10-30 02:32:21 -0500

    Add Categoria model with properties for category management

---
+++ Gestion_Productos_Login_Roles/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestion_Productos_Login_Roles.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public required string Nombre { get; set; }
+    }
+}
