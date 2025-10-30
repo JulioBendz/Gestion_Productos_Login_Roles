be5763bc9ed315e1ab0792233a52bd89e49c699a
JulioBendz <bengutjul2@gmail.com>
2025-10-30 02:31:23 -0500

    Add initial project structure with HomeController, views, and configuration files

---
+++ Gestion_Productos_Login_Roles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestion_Productos_Login_Roles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
